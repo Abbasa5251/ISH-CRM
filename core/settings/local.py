bbd39727889f5d4232796d12187bf5d1f9947334
Abbasa5251 <abbasa5251@hotmail.com>
2023-05-27 14:27:15 +0530

    added postgres support

--- core/settings/local.py
+++ core/settings/local.py
@@ -5,3 +5,10 @@ SECRET_KEY = 'django-insecure-!j#6ecz2j+2q1&o@#+x+m$48!x8^ztapaq0m+a!l#a+j#1snu0
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
--- core/settings/production.py
+++ core/settings/production.py
@@ -5,4 +5,15 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
