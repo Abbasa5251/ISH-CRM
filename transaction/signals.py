a57f8e21127d70fc9c452334d7e4b341daf114a9
Abbasa5251 <abbasa5251@hotmail.com>
2023-06-09 02:04:23 +0530

    feat: signal to handle deletion of transactions

--- transaction/signals.py
+++ transaction/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
 from transaction.models import Transaction
@@ -12,3 +12,12 @@ def update_payment_due(sender, instance, created, **kwargs):
             instance.customer.payment_due += instance.amount
 
         instance.customer.save()
+
+@receiver(pre_delete, sender=Transaction)
+def delete_transaction_update_payment_due(sender, instance, using, **kwargs):
+    if instance.type == "DEBIT":
+        instance.customer.payment_due += instance.amount
+    elif instance.type == 'CREDIT':
+        instance.customer.payment_due -= instance.amount
+    instance.customer.save()
+    
